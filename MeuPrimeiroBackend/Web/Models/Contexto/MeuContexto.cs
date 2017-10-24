5da3b3862a402cd962a7d34904dcda50e394a521
Eduardo Coning <eduardoconing@gmail.com>
2017-10-23 22:03:59 -0200

    Desat.

---
+++ MeuPrimeiroBackend/Web/Models/Contexto/MeuContexto.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Web.Models.Contexto
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base("strConn")
+        {
+
+
+
+        }
+
+        public System.Data.Entity.DbSet<Web.Models.TipoItem> TipoItems { get; set; }
+
+        public System.Data.Entity.DbSet<Web.Models.Item> Items { get; set; }
+    }
+}
